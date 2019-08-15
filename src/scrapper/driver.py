8cb629bb86daf392cdafd067e5f1add010f2b65b
Carles Bruguera <carlesba@gmail.com>
2019-08-15 12:27:16 +0200

    Reenable shm usage

--- src/scrapper/driver.py
+++ src/scrapper/driver.py
@@ -141,7 +141,6 @@ def new(*args, **kwargs):
     if headless:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-impl-side-painting")
